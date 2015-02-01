910ee7ddb1bc0a94251c104c54f61a7dd6abbe4e
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2015-02-01 12:21:59 +0100

    Changed the plugin description

--- wp-posts-lists-shortcode.php
+++ wp-posts-lists-shortcode.php
@@ -1,16 +1,16 @@
 <?php
 /* Plugin Name: WP Posts List Shortcode
  * Plugin URI: https://github.com/raymonschouwenaar/wp-posts-list-shortcode
- * Description: Replace [recent] with a short latest posts list
+ * Description: [posts-list] gives a list of the most recent posts
  * Version: 0.1.0
  * Author: Raymon Schouwenaar
  * Author URI: https://raymonschouwenaar.nl
  */
 function rss_posts_lists_shortcode()
 {
-    $out = '<ul>\n';
     $recentPosts = new WP_Query();
     $recentPosts->query( 'posts_per_page=5' );
+    $out = '<ul class="rss-wp-posts-list">\n';
     while ( $recentPosts->have_posts() ) {
         $recentPosts->the_post();
         $out .= '<li>\n'.
